2075a11d83fd533de974bec12cfe94539097cf74
ladasedlacek <sedlacek.lada@gmail.com>
2021-11-28 09:53:57 +0100

    Pre-final intro page + content of sub pages

--- js/script.js
+++ js/script.js
@@ -11,16 +11,21 @@ $(document).ready(function() {
         $("#testLayout").addClass("layout-1").css({'width':'1310px', 'margin':'0 auto'}), $("body.mobile").removeClass("mobile");
     }),
 
-    // accordion
-    $("#accordion > .lpFAQs__wrapper > .lpFAQs__paragraph").hide(),
-    $("#accordion > .lpFAQs__wrapper").click(function (e) {
-        if (!$(e.target).closest(".lpFAQs__headline").length) return;
-        return (
-            $(this).hasClass("active")
-                ? $(this).removeClass("active").find(".lpFAQs__paragraph").slideUp()
-                : ($("#accordion > .lpFAQs__wrapper > .lpFAQs__paragraph").slideUp(), $("#accordion > .lpFAQs__wrapper.active").removeClass("active"), $(this).addClass("active").find(".lpFAQs__paragraph").slideDown()),
-            !1
-        );
+    // tabs switcher
+    $("#landingpage .lpMainNav__tab").on("click", function() {
+        let saveTarget = $(this).attr('data-tab')
+        $("#landingpage .lpMainNav__tab").removeClass('tabActive')
+        $("#landingpage .lpMain").removeClass('mainActive')
+        $(this).addClass('tabActive')
+        $("#landingpage #" + saveTarget).addClass('mainActive')
+    }),
+
+    $("#landingpage .categoryTile").on("click", function() {
+        let saveTarget = $(this).attr('data-tab')
+        $("#landingpage .lpMainNav__tab").removeClass('tabActive')
+        $("#landingpage .lpMain").removeClass('mainActive')
+        $("#landingpage .lpMainNav__wrapper").find(`[data-tab='${saveTarget}']`).addClass('tabActive')
+        $("#landingpage #" + saveTarget).addClass('mainActive')
     }),
 
     // smooth scroller
@@ -28,16 +33,9 @@ $(document).ready(function() {
     .not('[href="#"]')
     .not('[href="#0"]')
     .click(function (a) {
-        var o;
+        let o;
         location.pathname.replace(/^\//, "") != this.pathname.replace(/^\//, "") ||
             location.hostname != this.hostname ||
-            ((o = (o = $(this.hash)).length ? o : $("[name=" + this.hash.slice(1) + "]")).length && (a.preventDefault(), $("html, body").animate({ scrollTop: o.offset().top }, 800)));
+            ((o = (o = $(this.hash)).length ? o : $("[name=" + this.hash.slice(1) + "]")).length && (a.preventDefault(), $("html, body").animate({ scrollTop: o.offset().top - 75 }, 800)));
     });
 });
-
-// on load
--1 < window.location.href.indexOf("alza")
-? $(window).load(function () {
-	  $("#landingpage").addClass("lpActive");
-})
-: $("#landingpage").addClass("lpActive");
